bb1e6653194859f61001a8e83b38a499c58e1b05
Wasee Malik <wmalik@mux.com>
2023-05-11 12:06:10 -0700

    Make useSpaceEvent less forgiving since we can decide when to listen to events now

--- hooks/useSpaceEvent.tsx
+++ hooks/useSpaceEvent.tsx
@@ -5,18 +5,24 @@ import { useContext, useEffect, useRef } from "react";
 /**
  * @param event
  * @param callback called whenever the event is fired. Warning: The callback should be provided using the `useCallback` hook to avoid this hook being called at every re-render of the component.
+ * @param startListening if false, the callback will not be registered until this is set to true. Useful for registering callbacks conditionally.
  */
 export const useSpaceEvent = (
   event: SpaceEvent,
-  callback: (...args: any) => void
+  callback: (...args: any) => void,
+  startListening = true
 ): void => {
   const { space } = useContext(SpaceContext);
 
   useEffect(() => {
-    if (space) {
-      space.on(event, callback);
-    } else {
-      console.warn(
+    if (!startListening) {
+      return;
+    }
+
+    try {
+      space?.on(event, callback);
+    } catch (_error) {
+      throw new Error(
         `Unable to register useSpaceEvent callback ${callback.name} as the Space does not exist yet.`
       );
     }
@@ -24,5 +30,5 @@ export const useSpaceEvent = (
     return () => {
       space?.off(event, callback);
     };
-  }, [event, callback, space]);
+  }, [event, callback, space, startListening]);
 };
